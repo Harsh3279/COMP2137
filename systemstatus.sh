325cfa5229607919eebac6190a3af7bb476aeb63
harsh <sadhu.harsh0095@gmail.com>
2025-05-14 15:29:55 -0400

    First script created

---
+++ systemstatus.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# show cpu activity
+echo "cpu activity"
+
+uptime
+
+echo
+
+# free up memory
+echo "memory usuage"
+free -h
+
+echo
+
+
+#free up disk space
+echo "disk space"
+df -h
